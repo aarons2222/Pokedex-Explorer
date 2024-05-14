762328f62515b87115cf756a55ebe6595db0c1cb
astric87 <aaron.strickland@icloud.com>
2024-05-14 10:21:05 +0100

    Intial upload

---
+++ Pokedex Explorer/Pokedex_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_ExplorerApp.swift
+//  Pokedex Explorer
+//
+//  Created by Aaron Strickland on 13/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
